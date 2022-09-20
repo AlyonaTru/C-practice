21d4745c5ed777fed41ca3b294286da8f8b06ec8
Alyona <mirka-sobaka@inbox.ru>
2022-09-20 11:15:06 +0400

    добавила папку для домашней работы

---
+++ Homework/002/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b;
+if (a > b)
+{
+        Console.WriteLine("наибольшее число = : " + a);
+}
+
+Console.WriteLine("наибольшее число = : " + b);
